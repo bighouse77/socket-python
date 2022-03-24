b1f756f975f6957939cff1afb19b744640b50a5c
Maurício Casagrande <61338766+bighouse77@users.noreply.github.com>
2022-03-24 19:56:01 -0300

    Update and rename socket.py to servidor.py
    att

---
+++ servidor.py
@@ -0,0 +1,15 @@
+# Importando o módulo que possibilita abrir conexões
+import socket
+
+# Objeto que será o socket UDP para IPv4 do servidor
+socketServidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Vincular um IP e uma porta do socket
+socketServidor.bind(('localhost', 5000))
+
+while True:
+    # Receber possíveis pacotes de até 1024
+    mensagem = socketServidor.recvfrom(1024)
+    
+    # O objeto é uma tupla (conteúdo, (IP, porta))
+    print("Recebido", mensagem[0], "de", mensagem[1])
