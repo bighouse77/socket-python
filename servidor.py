91b2339b9d8f9df0007ee05aaed31b3b05f256f1
Maurício Casagrande <61338766+bighouse77@users.noreply.github.com>
2022-03-24 20:26:40 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -7,9 +7,16 @@ socketServidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Vincular um IP e uma porta do socket
 socketServidor.bind(('localhost', 5000))
 
+print("Servidor inicial na porta 5000, escutando mensagens")
+
 while True:
     # Receber possíveis pacotes de até 1024
     mensagem = socketServidor.recvfrom(1024)
     
     # O objeto é uma tupla (conteúdo, (IP, porta))
     print("Recebido", mensagem[0], "de", mensagem[1])
+    
+    if mensagem[0] == b"sair":
+        break
+    
+socketServidor.close()
