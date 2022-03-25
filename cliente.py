33dd17f84e8edc0b0bf384425958de4e2bf77a8c
Maurício Casagrande <61338766+bighouse77@users.noreply.github.com>
2022-03-24 21:56:26 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -7,24 +7,24 @@ Created on Thu Mar 24 19:38:51 2022
 
 #Importar o módulo socket
 
-import socket
+import socket, json
 
 # Criar um socket UDP para IPv4
 socketCliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+objetoPython = {'nome': 'Mauricio', 'nota': 7.2, 'ra': 123}
+
+#Serializar o objeto no formato json
+dadoJson = json.dumps(objetoPython)
+
+print('Após serialização: ', dadoJson)
+
+# Os dados devem ser codificados em vetores de bytes
+dados = str.encode(dadoJson)
+
 # Endereço e porta do servidor
 endereco = ('localhost', 5000)
 
-while True:
-    texto = input(">")
-
-    # Os dados devem ser codificados em vetores de bytes
-    dados = str.encode(texto)
+# Enviar uma mensagem
+socketCliente.sendto(dados, endereco)
     
-    # Enviar uma mensagem
-    socketCliente.sendto(dados, endereco)
-    
-    if texto == "sair":
-        break
-    
-socketCliente.close()
